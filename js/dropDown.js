6407e0a220a72e042caacceda41a902540f653f6
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2019-03-10 12:27:02 +0100

    mobil nav fix 4

--- js/dropDown.js
+++ js/dropDown.js
@@ -1,10 +1,17 @@
-// dropdown function 
+// nav menu
 
-function dropDown(y) {
-    let x = document.getElementById(y);
-    if (x.style.display === "flex") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "flex";
-    }
+
+function dropDown(x, y, z) {
+  let nav = document.getElementById(x);
+  let icon1 = document.getElementById(y);
+  let icon2 = document.getElementById(z);
+  if (nav.style.display === "flex") {
+    nav.style.display = "none";
+    icon1.style.display = "flex";
+    icon2.style.display = "none";
+  } else {
+    nav.style.display = "flex";
+    icon1.style.display = "none";
+    icon2.style.display = "flex";
   }
+}
